0f6fe3abe54ad8c407f34df270a21c4fb68e1d38
dokclayton <133478498+dokclayton@users.noreply.github.com>
2023-05-27 19:18:04 +0800

    Update server.js

--- server.js
+++ server.js
@@ -15,7 +15,7 @@ const db = knex({
 
 const app = express();
 
-let intialPath = path.join(__dirname, "public");
+let intialPath = path.join(__dirname, "doc");
 
 app.use(bodyParser.json());
 app.use(express.static(intialPath));
